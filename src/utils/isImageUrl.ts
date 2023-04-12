35183cfa1d4dc118e98ee0546431aa374bf4d876
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2023-04-12 23:37:23 +0200

    basic changes

--- src/utils/isImageUrl.ts
+++ src/utils/isImageUrl.ts
@@ -2,7 +2,7 @@
  * Determines whether a given URL is an image URL.
  *
  * @param {string} url - The URL to check.
- * @returns {boolean} True if the URL starts with https://"
+ * @returns {boolean} True if the URL starts with https://
  */
 export const isImageUrl = (url: string): boolean => {
   return url.startsWith("https://");
